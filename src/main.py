283a2a77231f79e454434a5fd2744f838d70952f
Fireclaw <fireclawthefox@gmail.com>
2014-10-28 10:45:36 +0100

    change space<>tab differences in some files

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ loadPrcFileData("",
     window-title GrimFang OWP - Dungeon Crawler
     cursor-hidden 0
     show-frame-rate-meter 1
-	#win-size 1024 600
+    #win-size 1024 600
     #fullscreen #t
     model-path $MAIN_DIR/assets/
 """
@@ -12,47 +12,68 @@ loadPrcFileData("",
 
 from direct.showbase.ShowBase import ShowBase
 from gui.MainMenu import MainMenu
+from gui.HostMenu import HostMenu
+import socket
 
 class Main(ShowBase):
-	"""Main class of the application
-	initialise the engine (ShowBase)"""
+    """Main class of the application
+    initialise the engine (ShowBase)"""
 
-	def __init__(self):
-		"""initialise the engine"""
-		ShowBase.__init__(self)
-		self.setBackgroundColor(0,0,0)
+    def __init__(self):
+        """initialise the engine"""
+        ShowBase.__init__(self)
+        self.setBackgroundColor(0,0,0)
 
-		self.mainMenu = MainMenu()
+        self.mainMenu = MainMenu()
+        self.hostMenu = HostMenu()
+        self.hostMenu.hide()
 
-		## Basic events
-		self.acceptAll()
+        ## Basic events
+        self.acceptAll()
 
-	def host(self):
-		"""starting the server"""
-		# instantiate the server
-		from server.server import DungeonServerRepository
-		self.sr = DungeonServerRepository()
-		# instantiate the Level
-		from client.client import LevelAIRepository
-		print "start the Level AI"
-		self.levelAI = LevelAIRepository()
+    def host(self):
+        """starting the server"""
+        self.hostMenu.show()
+        # instantiate the server
+        from server.server import DungeonServerRepository
+        self.sr = DungeonServerRepository()
+        # instantiate the Level
+        from client.client import LevelAIRepository
+        base.messenger.send("addLog", ["start the Level AI"])
+        self.levelAI = LevelAIRepository()
+        ip = [(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
+        base.messenger.send("addLog",
+            ["Server at: {} - Running - Version 1.0".format(ip)])
 
-	def join(self, ip):
-		from client.client import Client
-		self.client = Client(ip)
+    def stopHost(self):
+        self.mainMenu.show()
+        del self.levelAI
+        self.levelAI = None
+        del self.sr
+        self.sr = None
 
-	def acceptAll(self):
-		"""Accept all events that have to be catched by the main class"""
-		self.accept("escape", self.quit)
-		self.accept("start_server", self.host)
-		self.accept("start_client", self.join)
+    def join(self, ip):
+        from client.client import Client
+        self.client = Client(ip)
+
+    def stopJoin(self):
+        #??? self.client.stop()
+        pass
+
+    def acceptAll(self):
+        """Accept all events that have to be catched by the main class"""
+        self.accept("escape", self.quit)
+        self.accept("start_server", self.host)
+        self.accept("stop_server", self.stopHost)
+        self.accept("start_client", self.join)
+        self.accept("stop_client", self.stopJoin)
 
 
-	def quit(self):
-		if self.appRunner:
-			self.appRunner.stop()
-		else:
-			exit(0)
+    def quit(self):
+        if self.appRunner:
+            self.appRunner.stop()
+        else:
+            exit(0)
 
 # instantiate the engine
 base = Main()
